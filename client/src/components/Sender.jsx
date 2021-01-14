20a9f905cafbafeee2f881605ab2683a7e75520f
Oleksandr <olysiuk01@gmail.com>
2021-01-14 15:46:57 +0200

    Sender onKeyDown(enter)

--- client/src/components/Sender.jsx
+++ client/src/components/Sender.jsx
@@ -1,22 +1,27 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 
 const Sender = ({onChangeMessage, sendMessage, message}) => {
 
-    const onClickBtn = () =>{
+    const onSubmit = () => {
         sendMessage()
         const el = document.getElementById('senderInput')
         el.focus()
+    }
+
+    const onPressEnter = (e) =>{
+        if(e.key !== 'Enter') return;
+        onSubmit()
     }
 
     return (
         <section className='sender'>
             <div className="container">
                 <div className="sender__wrapper">
-                    <div className="sender__input">
+                    <div className="sender__input" onKeyDown={onPressEnter}>
                         <input id='senderInput' type="text" placeholder='Type a new message' value={message}
                                onChange={(e) => onChangeMessage(e.target.value)}/>
                     </div>
-                    <div className="sender__btn" onClick={onClickBtn}>Send</div>
+                    <div className="sender__btn" onClick={onSubmit}>Send</div>
                 </div>
             </div>
         </section>
