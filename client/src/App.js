e71e2c1c78654f539fb4043ed45c60d41341ad25
Oleksandr <olysiuk01@gmail.com>
2021-01-15 17:43:24 +0200

    login

--- client/src/App.js
+++ client/src/App.js
@@ -9,120 +9,53 @@ const CONNECTION_PORT = 'localhost:8000/'
 function App() {
 
     const [loggedIn, setLoggedIn] = useState(false)
-    const [userName, setUserName] = useState('')
     const [userData, setUserData] = useState(null)
-    const [message, setMessage] = useState('')
-    const [messageList, setMessageList] = useState([])
-    const [activeUsers, setActiveUsers] = useState([])
-
-    const onChangeUserName = (name) => {
-        setUserName(name)
-    }
-
-    const onChangeMessage = (message) => {
-        setMessage(message)
-    }
-
-    const sendMessage = async () => {
-        if (message.trim().length) {
-            let messageContent = {
-                room: 'room',
-                content: {
-                    author: userName,
-                    authorId: userData.id,
-                    messageId: Math.floor(Math.random() * 100000000),
-                    status: 'sent',
-                    message
-                }
-            }
-
-            await socket.emit("send_message", messageContent)
-            setMessageList([...messageList, messageContent.content])
-            setMessage('')
-        }
-    }
-
-    const connectToRoom = () => {
-        setLoggedIn(true)
-        socket.emit('join', {room: 'room', user: userName})
-    }
-
-    const readMessage = async (messageId) => {
-        const messageIndex = messageList.findIndex((i) => i.messageId === messageId)
-        if (messageIndex) {
-
-            //  const msg = Object.assign({}, messageList[messageIndex])
-            // msg.status = 'read'
-            // const newArr = messageList.filter((item, index) => index !== messageIndex)
-            // setMessageList([...newArr, msg])
-            // console.log(messageList)
-            // await socket.emit("read_message", msg)
-
-            let msgs = [...messageList]
-            const msg = Object.assign({}, messageList[messageIndex])
-            msg.status = 'read'
-            msgs.splice(messageIndex, 1, msg)
-            setMessageList(msgs)
-            await socket.emit('read_message', msg)
-
-        }
-    }
 
     useEffect(() => {
         socket = io(CONNECTION_PORT)
     }, [])
 
+    // CONNECT
+    const connectTo = (userName) => {
+        if (userName && userName.trim().length) {
+            socket.emit('join', userName)
+        }
+    }
+
+    // SEND MESSAGE
+
+    const sendMessage = (message) => {
+        if (message && message.trim().length) {
+            socket.emit('send_message', message)
+        }
+    }
+
+
+    // useEffect HOOKS
 
     useEffect(() => {
-        socket.on('receive_message', (data) => {
-            // const msg = messageList.length && messageList.find((i) => i.messageId === data.messageId)
-            // if (!msg) {
-            //     console.log('DATA:', data)
-            // console.log('PREV:', [...messageList])
-            setMessageList([...messageList, data])
-            // console.log([...messageList, data])
-            // }
-        })
-    }, [messageList])
-
-
-    useEffect(() => {
-        socket.on('set_users', (data) => {
-            setActiveUsers(data)
-        })
-    }, [])
-
-    useEffect(() => {
-        socket.on('set_user', (data) => {
-            setUserData(data)
+        socket.on('set_user', (user) => {
+            setUserData(user)
         })
     }, [])
 
+
     useEffect(() => {
-        socket.on('receive_read_message', (data) => {
-            const messageIndex = messageList.findIndex((i) => i.messageId === data.messageId)
-            // const newArr = messageList.filter((item, index) => index !== messageIndex)
-            //
-            // setMessageList([...newArr, data])
-
-            let msgs = [...messageList]
-            msgs.splice(messageIndex, 1, data)
-            setMessageList(msgs)
-
-
-        })
-    }, [messageList])
+        if (userData) {
+            setLoggedIn(true)
+        }
+    }, [userData])
 
     return (
         <div className='wrapper'>
             {!loggedIn ? (
-                <Login onChangeUserName={onChangeUserName} connectToRoom={connectToRoom}/>
+                <Login connectTo={connectTo}/>
             ) : (
                 <div>
-                    <Aside userName={userName} activeUsers={activeUsers}/>
+                    <Aside/>
                     <main className="main">
-                        <Dialog messages={messageList} userData={userData} readMessage={readMessage}/>
-                        <Sender onChangeMessage={onChangeMessage} sendMessage={sendMessage} message={message}/>
+                        <Dialog/>
+                        <Sender sendMessage={sendMessage}/>
                     </main>
                 </div>
             )}
--- client/src/components/Login.jsx
+++ client/src/components/Login.jsx
@@ -1,16 +1,30 @@
-import React from 'react';
+import React, {useState} from 'react';
 
-const Login = ({onChangeUserName, connectToRoom}) => {
+const Login = ({connectTo}) => {
 
+    const [userName, setUserName] = useState('')
+
+    const onChangeUserName = (e) => {
+        setUserName(e.target.value)
+    }
+
+    const onKeyDown = (e) => {
+        if (e.key !== 'Enter') return;
+        onSubmit()
+    }
+
+    const onSubmit = () =>{
+        connectTo(userName)
+    }
 
 
     return (
-        <section className="login">
+        <section className="login" onKeyDown={onKeyDown}>
             <div className="login__wrapper">
                 <div className="login__content">
                     <h1>Please, enter your username</h1>
-                    <input type="text" placeholder='name' onChange={(e)=> onChangeUserName(e.target.value)}/>
-                    <button type="submit" onClick={connectToRoom}>Enter</button>
+                    <input type="text" placeholder='name' onChange={onChangeUserName}/>
+                    <button type="button" onClick={onSubmit}>Enter</button>
                 </div>
             </div>
         </section>
